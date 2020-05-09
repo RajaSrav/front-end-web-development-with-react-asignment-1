1baa4e1708ff95a7da128daa5381974427e673bf
Raja's <gkrajareddy@gmail.com>
2020-05-09 18:14:22 +0530

    Updates

---
+++ precache-manifest.4ef482633e37fc20335047b3ac3c681f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1e1771db6f674cdde029cf5789114d",
+    "url": "/front-end-web-development-with-react-asignment-1/index.html"
+  },
+  {
+    "revision": "0fd0f517000c005533dc",
+    "url": "/front-end-web-development-with-react-asignment-1/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2044b447305f761ff80a",
+    "url": "/front-end-web-development-with-react-asignment-1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0fd0f517000c005533dc",
+    "url": "/front-end-web-development-with-react-asignment-1/static/js/2.732368fa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/front-end-web-development-with-react-asignment-1/static/js/2.732368fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2044b447305f761ff80a",
+    "url": "/front-end-web-development-with-react-asignment-1/static/js/main.8123cdc3.chunk.js"
+  },
+  {
+    "revision": "3b5cabfe5f86a3a655c6",
+    "url": "/front-end-web-development-with-react-asignment-1/static/js/runtime-main.4e16fbca.js"
+  }
+]);
